8001cf22ee052686fe0ba9b3b52b902f41773a5b
Nadya <meng.meiyingg@gmail.com>
2023-05-06 19:54:03 +0300

    added swiper

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
   new WOW().init();
 })
 
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
