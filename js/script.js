af7ff50604c07b38d9b5e69d9115e68b2984e84f
Nadya <meng.meiyingg@gmail.com>
2023-05-20 20:40:54 +0300

    added yandex map

--- js/script.js
+++ js/script.js
@@ -1,20 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
   new WOW().init();
+
+
+    // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map-container", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [59.94576038603253, 30.327023798291457],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 13
+        });
+        var myPlacemark = new ymaps.Placemark([59.938955, 30.315644], {
+          iconContent: 'САНКТ-ПЕТЕРБУРГ',
+          balloonContent: 'Столица России',
+        }, {
+          iconLayout: "default#image",
+          iconImageHref: "../img/point.svg",
+          iconImageSize: [21, 56],
+          iconImageOffset: [0, -76]
+        });
+        myMap.geoObjects.add(myPlacemark);
+    }
+
+    const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      loop: true,
+    
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      a11y: {
+        prevSlideMessage: 'Предыдущий слайд',
+        nextSlideMessage: 'Следующий слайд',
+      },
+    });
 })
 
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  a11y: {
-    prevSlideMessage: 'Предыдущий слайд',
-    nextSlideMessage: 'Следующий слайд',
-  },
-});
